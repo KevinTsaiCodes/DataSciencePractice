51fb7b50d1e6e1378fb933d1bed8873a8799ebe9
KevinTsaiCodes <KevinTsaiCodes>
2024-05-22 21:10:51 +0800

    add some new

--- ImageProcessing/ch2/image_acquisition.py
+++ ImageProcessing/ch2/image_acquisition.py
@@ -1,1 +1,25 @@
 import argparse
+import cv2
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="A CLI tool for reading, showing and writing the image.")
+    parser.add_argument("-i", "--input_image", description="path/to/the/input/image/file", default="figs/lena.jpg")
+    return parser.parse_args()
+
+
+def read_image(filename):
+    file = cv2.imread(filename)
+    is_exist = file
+    if is_exist == 'None':
+        print(f"{filename} not found")
+        exit(1)
+
+
+def main(opt):
+    pass
+
+
+
+if __name__ == '__main__':
+    opt = parse_args()
+    main(opt)
